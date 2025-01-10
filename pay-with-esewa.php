e83bb796c6c072e52b62e0ace236d758a54a47dc
Er. Ranjit Kumar Das <120443204+ranjitology@users.noreply.github.com>
2025-01-10 07:43:28 +0545

    Create pay-with-esewa.php

---
+++ pay-with-esewa.php
@@ -0,0 +1,19 @@
+<?php
+// pay-with-esewa.php
+
+$esewa_merchant_id = "your_merchant_id";
+$amount = $_GET['amount'];
+$product_name = $_GET['product'];
+$return_url = "http://your-website.com/payment-success.php";
+$cancel_url = "http://your-website.com/payment-failed.php";
+
+header("Location: https://esewa.com.np/epay/main?"
+    . "amt=$amount"
+    . "&p1=product"
+    . "&p2=$product_name"
+    . "&p3="
+    . "&p4=$esewa_merchant_id"
+    . "&su=$return_url"
+    . "&fu=$cancel_url");
+exit();
+?>
